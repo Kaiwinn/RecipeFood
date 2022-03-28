b0fd50c62a6f9ae2dee3a8748044085d904a7380
KaiWin <cn764011@gmail.com>
2022-03-28 17:47:16 +0700

    ABC

--- app/src/main/java/com/example/recipefoodapp/MainActivity.java
+++ app/src/main/java/com/example/recipefoodapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.recipefoodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -27,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     Spinner spinner;
     List<String> tags = new ArrayList<>();
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +38,23 @@ public class MainActivity extends AppCompatActivity {
         // initialize
         dialog = new ProgressDialog(this);
         dialog.setTitle("Loading...");
+
+        searchView = findViewById(R.id.searchView_home);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                tags.clear();
+                tags.add(query);
+                manager.getRandomRecipes(randomRecipeResponseListeners, tags);
+                dialog.show();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
 
         spinner = findViewById(R.id.spinner_tags);
         ArrayAdapter arrayAdapter = ArrayAdapter.createFromResource(
